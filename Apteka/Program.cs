d5d4bfff0870a29e1971c5a9af7aacd360a11012
Филипп Баннов <bannov.2002@bk.ru>
2022-05-11 09:21:49 +0300

    Добавьте файлы проекта.

---
+++ Apteka/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Apteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cc = 0;
+            List<lecarstva> lec = new List<lecarstva>();
+            lec.Add(new lecarstva(1, "Нурофен", "Рекитт", "177 руб.", 50, "Да"));
+            lec.Add(new lecarstva(2, "Пенталгин", "Фармстандарт", "119 Руб.", 27, "Да"));
+            lec.Add(new lecarstva(3, "Лазолван", ", Дельфарм", "228 Руб.",  40, "Да"));
+            lec.Add(new lecarstva(4, "Корвалол Реневал", "Обновление", "122 Руб.", 30, "Да"));
+            lec.Add(new lecarstva(5, "Амбробене сироп", "Меркле", "133 Руб.", 10, "Да"));
+            foreach (var Komplectyushie in lec)
+            {
+                Console.WriteLine(Komplectyushie);
+            }
+            Console.WriteLine("Выберите команду:");
+            Console.WriteLine("1- Получение списка по наванию лекарства");
+            Console.WriteLine("2- Получение списка по наванию производителя");
+            Console.WriteLine("3- Получение самого дорогого лекарства");
+            Console.WriteLine("4- Сортировка товара по возрастанию цены ");
+            Console.WriteLine("5- Сортировка товара по убыванию цены ");
+            Console.WriteLine("6- Список формацевтов родившихся в определённом году");
+            if ((!int.TryParse(Console.ReadLine(), out var cam)) || cam < 1 || cam > 6)
+            {
+                Console.WriteLine("Ошибка, недопустимы диапазон!");
+            }
+            else
+            {
+                if (cam == 1)
+                {
+                    int b = 0;
+                    Console.WriteLine("Введите название лекарства:");
+                    string s = Console.ReadLine();
+                    var sorted1 = from p in lec
+                                  orderby p.Name
+                                  select p;
+
+                    foreach (var p in sorted1)
+                    {
+
+                        if (s == p.Name)
+                        {
+                            cc = p.Colichestvo;
+                            Console.WriteLine($"Параметр: {p.Parametr}, Наименование: {p.Name}, Производитель: {p.Proizvoditel}, Цена: {p.Price}руб., Количество:{p.Colichestvo}, Активный:{p.Activity}");
+                            b++;
+                        }
+                    }
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Такого товара нет!");
+                    }
+                }
+                if (cam == 2)
+                {
+                    int b = 0;
+                    Console.WriteLine("Введите название производителя:");
+                    string s = Console.ReadLine();
+                    var sorted2 = from p in lec
+                                  orderby p.Proizvoditel
+                                  select p;
+                    foreach (var p in sorted2)
+                    {
+                        if (s == p.Proizvoditel)
+                        {
+                            Console.WriteLine($"Производитель: {p.Proizvoditel}");
+                            b++;
+                        }
+                    }
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Такого производителя нет!");
+                    }
+                }
+
+                if (cam == 3)
+                {
+
+                    var sorted2 = from p in lec
+                                  orderby p.Price, p.Proizvoditel
+                                  select p;
+                    foreach (var p in sorted2)
+                        Console.WriteLine(p);
+                }
+
+                if (cam == 4)
+                {
+
+                    var sorted2 = from p in lec
+                                  orderby p.Price descending, p.Proizvoditel
+                                  select p;
+                    foreach (var p in sorted2)
+                        Console.WriteLine(p);
+                }
+
+                Console.WriteLine("Введите название товара для покупки:");
+                string ss = Console.ReadLine();
+                var sorted3 = from r in lec
+                              orderby r.Name
+                              select r;
+
+                foreach (var r in sorted3)
+                {
+                    if (ss == r.Name)
+                    {
+                        cc = r.Colichestvo;
+                        Console.WriteLine($"Параметр: {r.Parametr}, Наименование: {r.Name}, Описание: {r.Opisanie}, Производитель: {r.Proizvoditel}, Цена: {r.Price}руб., Активный:{r.Activity}, Количество:{r.Colichestvo}");
+                    }
+                }
+                Console.WriteLine("Введите количество товара:");
+                if ((!int.TryParse(Console.ReadLine(), out var call)) || call < 1 || call > cc)
+                {
+                    Console.WriteLine("Ошибка!");
+                }
+                else
+                {
+                    Console.WriteLine("Выберите вид оплаты:");
+                    Console.WriteLine("1- Банковскаякарта");
+                    Console.WriteLine("2- Google pay");
+                    Console.WriteLine("3- Apple pay");
+                    Console.WriteLine("4- Наличные");
+                    if ((!int.TryParse(Console.ReadLine(), out var plata)) || plata < 1 || plata > 4)
+                    {
+                        Console.WriteLine("Ошибка!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите ваше ФИО и номер телефона");
+                        string fioPh = Console.ReadLine();
+                        Console.WriteLine("Выберите вид доставки:");
+                        Console.WriteLine("1- Самовывоз");
+                        Console.WriteLine("2- Доставка");
+                        Console.WriteLine("3- Срочная доставка");
+                        if ((!int.TryParse(Console.ReadLine(), out var dostavka)) || dostavka < 1 || dostavka > 3)
+                        {
+                            Console.WriteLine("Ошибка!");
+                        }
+                        else
+                        {
+                            if (dostavka == 2 || dostavka == 3)
+                            {
+                                Console.WriteLine("Введите адрес(улица, дом квартира):");
+                                string adres = Console.ReadLine();
+                                Console.WriteLine("Заказ оформлен!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ошибка");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Apteka/lecarstva.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apteka
+{
+   public class lecarstva
+    {
+        public lecarstva(int parametr, string name, string proizvoditel, string price, int colichestvo, string activity)
+        {
+            Parametr = parametr;
+            Name = name;
+            Proizvoditel = proizvoditel;
+            Price = price;
+            Colichestvo = colichestvo;
+            Activity = activity;
+        }
+        public int Parametr { get; set; }
+        public string Name { get; set; }
+        public string Opisanie { get; set; }
+        public string Proizvoditel { get; set; }
+        public string Price { get; set; }
+        public int Colichestvo { get; set; }
+        public string Activity { get; set; }
+        public override string ToString()
+        {
+            return "Параметр товара" + " " + Parametr + " " + "Наименование товара:" + Name + " "  + "Производитель:" + Proizvoditel + " " + "Цена:" + Price + " " + "Количество" + " " + Colichestvo + "Актуальный?" + Activity + " ";
+        }
+        public override bool Equals(object Tovar) /*Осуществление на то что значения не повторялись и пуст ли список?*/
+        {
+            if (Tovar == null) return false;
+            lecarstva objAsPart = Tovar as lecarstva;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public override int GetHashCode()
+        {
+            return Parametr;
+        }
+        public bool Equals(lecarstva other)
+        {
+            if (other == null) return false;
+            return (this.Parametr.Equals(other.Parametr));
+        }
+    }
+}
